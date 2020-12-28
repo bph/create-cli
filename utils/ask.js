8ad251d483bbcffc4eadd0f775602f121a374d4f
bph <birgit.pauli@gmail.com>
2020-12-28 16:44:56 -0500

    we create history for certain inputs

--- utils/ask.js
+++ utils/ask.js
@@ -1,17 +1,32 @@
 const fs = require('fs');
+const path = require('path');
 const { Input } = require('enquirer');
 const to = require('await-to-js').default;
 const handleError = require('cli-handle-error');
 const shouldCancel = require('cli-should-cancel');
+const { Store } = require('data-store');
+
 
 module.exports = async ({name, message, hint, initial}) => {
- 
+        let history = false; 
+        if(
+            !initial && 
+            name !== `name` && 
+            name !== `command` && 
+            name !== `description`
+        ) {
+            history = {
+                store: new Store({ path: `${__dirname}/.history/${name}.json`}),
+                autosave: true
+            }
+        }
         const [err, response] = await to (
             new Input({
                 name,
                 message,
                 hint,
                 initial,
+                history,
                 validate(value, state){
                    
                     if (state && state.name === `command`) return true; // no npm dedupe necessary :-) we just leave the value empty
