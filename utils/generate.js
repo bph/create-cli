14f1669d8e7776ab16fef4339f0e27cbca2037cf
bph <birgit.pauli@gmail.com>
2020-12-28 15:26:42 -0500

    Add dependencies, + prettier and scripts

--- utils/generate.js
+++ utils/generate.js
@@ -1,9 +1,11 @@
 const path = require('path');
 const copy = require('copy-template-dir');
-const { green: g, dim: d } = require('chalk');
+const { yellow: y, green: g, dim: d } = require('chalk');
 const alert = require('cli-alerts');
+const execa = require('execa');
 const questions = require('./questions');
-
+const ora = require('ora');
+const spinner = ora({text:''});
 module.exports = async () => {
 
     const vars = await questions();
@@ -11,7 +13,7 @@ module.exports = async () => {
     const inDirPath = path.join(__dirname, `../template`);
     const outDirPath = path.join(process.cwd(), outDir);
 
-    copy(inDirPath, outDirPath, vars, (err, createdFiles) => {
+    copy(inDirPath, outDirPath, vars, async (err, createdFiles) => {
         if (err) throw err;
         
         console.log(d(`\nCreating files in ${g(`./${outDir}`)} directory:\n`));
@@ -20,6 +22,22 @@ module.exports = async () => {
             const filename = path.basename(filePath);
             console.log(`Created: ${filename}`);
         })
+        //add it again for practice to add a spinner. It could be a great service to the developer using this cli-scaffold, to know that npm dedupe ran on the new directory. 
+
+        spinner.start(`${y(`Dependencies`)} installing ... \n\n${d(`It may take a moment or two.`)}`);
+        process.chdir(outDirPath);
+        // And now we need to install all the dependencies for the new cli. 
+        const pkgs = [
+            'meow',
+            'cli-meow-help',
+            'cli-welcome',
+            'cli-alerts',
+            'cli-handle-unhandled'
+        ]
+        await execa(`npm`, [`install`, ...pkgs]);
+        await execa(`npm`, [`install`, `prettier`, `-D`]);
+        await execa(`npm`, [`dedupe`]);
+        spinner.succeed(`${g(`Dependencies`)} installed.`)
 
         alert({
           type:`success`,
